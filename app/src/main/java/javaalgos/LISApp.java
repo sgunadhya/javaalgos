aac8c70f12d065fe021ff0e8f47ce927ddfce1f2
Sushant Srivastava <sushant.srivastav@gmail.com>
2021-05-17 01:44:31 +0530

    LIS

---
+++ app/src/main/java/javaalgos/LISApp.java
@@ -0,0 +1,25 @@
+package javaalgos;
+import java.util.*;
+
+public class LISApp {
+	
+	public static int maxMissilesIntercepted(int[] tests) {
+		assert tests != null;
+		assert tests.length > 0;
+		System.out.println(Arrays.toString(tests));
+		int n = tests.length;
+		int[] LIS = new int[n];
+		LIS[0] = 1;
+		int max = LIS[0];
+		for(int i = 1; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				if(tests[j] > tests[i]) {
+					LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+				}
+			}
+			max = Math.max(max, LIS[i]);
+		}
+		System.out.println(Arrays.toString(LIS));
+		return max;
+	}
+}
