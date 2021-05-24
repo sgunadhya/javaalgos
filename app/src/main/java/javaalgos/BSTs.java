9e73c97302399b379d6407d6c1385dc18082ea28
Sushant Srivastava <sushant.srivastav@gmail.com>
2021-05-24 14:27:14 +0530

    Added a valid BST

--- app/src/main/java/javaalgos/BSTs.java
+++ app/src/main/java/javaalgos/BSTs.java
@@ -1,4 +1,18 @@
 package javaalgos;
 
 public class BSTs {
+	
+	public static class BSTNode {
+		int data;
+		BSTNode left;
+		BSTNode right;
+		public BSTNode(int data) {
+			this.data = data;
+		}
+		public BSTNode(int data, BSTNode left, BSTNode right) {
+			this.data = data;
+			this.right = right;
+			this.left = left;
+		}
+	}
 }
--- app/src/test/java/javaalgos/BSTsTest.java
+++ app/src/test/java/javaalgos/BSTsTest.java
@@ -5,6 +5,22 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class BSTsTest {
+	BSTs.BSTNode a;
+	
+	@Before public void init() {
+		BSTs.BSTNode c = new BSTs.BSTNode(3,new BSTs.BSTNode(2, null, null), new BSTs.BSTNode(5, null, null));
+		BSTs.BSTNode f = new BSTs.BSTNode(11, null, new BSTs.BSTNode(17, new BSTs.BSTNode(13, null, null),null));
+		BSTs.BSTNode b = new BSTs.BSTNode(7, c, f);
+		BSTs.BSTNode i = new BSTs.BSTNode(43, 
+			new BSTs.BSTNode(23, null, 
+				new BSTs.BSTNode(37, 
+					new BSTs.BSTNode(29, null, 
+						new BSTs.BSTNode(31, null, null)), 
+							new BSTs.BSTNode(41, null, null))), 
+								new BSTs.BSTNode(47, null, new BSTs.BSTNode(53, null, null)));
+		a = new BSTs.BSTNode(19, b, i);
+		 
+	}
 	
 	@Test public void testIsValidBST() {
 		
