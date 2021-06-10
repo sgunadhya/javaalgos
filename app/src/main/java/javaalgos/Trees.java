a263ae88f68eb3466ca9dc6625ae8b86b284d179
Sushant Srivastava <sushant.srivastav@gmail.com>
2021-06-10 14:25:01 +0530

    Added code for Predecessor Array

--- app/src/main/java/javaalgos/Trees.java
+++ app/src/main/java/javaalgos/Trees.java
@@ -1,5 +1,10 @@
 package javaalgos;
 
+import java.util.*;
 public class Trees {
 	
+	public static Map<Integer, List<Integer>> toGraphFromArray(int[] p) {
+		return new HashMap<>();
+	}
+	 
 }
--- app/src/test/java/javaalgos/TreesTest.java
+++ app/src/test/java/javaalgos/TreesTest.java
@@ -6,4 +6,13 @@ import java.util.*;
 
 public class TreesTest {
 	
+	@Test
+	public void testToGraphFromPredecessorArray() {
+		int[] pA = new int[]{-1, 0,1,0,2,3};
+		Map<Integer, List<Integer>> g = Trees.toGraphFromArray(pA);
+		assertTrue(g.get(0).size() == 2);
+		assertTrue(g.get(1).size() == 1);
+		assertTrue(g.get(2).size() == 1);
+		assertTrue(g.get(3).size() == 1);
+	}
 }
