bb7bcf550a3779ad61e090ef6a5e36fe256d3c7a
Sushant Srivastava <sushant.srivastav@gmail.com>
2021-06-10 17:43:55 +0530

    Added code for HuffmanCoding

--- app/src/main/java/javaalgos/Trees.java
+++ app/src/main/java/javaalgos/Trees.java
@@ -3,6 +3,23 @@ package javaalgos;
 import java.util.*;
 public class Trees {
 	
+	public static class HuffmanNode implements Comparable<HuffmanNode> {
+		HuffmanNode left;
+		HuffmanNode right;
+		Character c;
+		int f;
+		public HuffmanNode(Character c, Integer f, HuffmanNode left, HuffmanNode right) {
+			this.left = left;
+			this.right = right;
+			this.c = c;
+			this.f = f;
+		}
+		
+		public int compareTo(HuffmanNode o) {
+			return Integer.compare(this.f, o.f);
+		}
+	}
+	
 	public static Map<Integer, List<Integer>> toGraphFromArray(int[] p) {
 		Map<Integer, List<Integer>> g = new HashMap<>();
 		for(int i = 1; i < p.length ; i++) {
