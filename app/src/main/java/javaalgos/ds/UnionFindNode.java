d241be4f39cd3faf3db0f6b31de98567f6dad557
Sushant Srivastava <sushant.srivastav@gmail.com>
2022-01-26 18:56:36 +0530

    Marking fields protected for package-access

--- app/src/main/java/javaalgos/ds/UnionFindNode.java
+++ app/src/main/java/javaalgos/ds/UnionFindNode.java
@@ -3,9 +3,9 @@ package javaalgos.ds;
 import javaalgos.adt.PartitionElement;
 
 public class UnionFindNode<T> implements PartitionElement<T> {
-    private UnionFindNode<T> parentNode;
-    private int rank;
-    private T data;
+    UnionFindNode<T> parentNode;
+    int rank;
+    T data;
 
     public UnionFindNode(T data) {
         this.data = data;
@@ -35,29 +35,5 @@ public class UnionFindNode<T> implements PartitionElement<T> {
     @Override
     public String toString() {
         return String.format("UnionFindNode{data=%s}", data);
-    }
-
-    public UnionFindNode<T> getParentNode() {
-        return parentNode;
-    }
-
-    public void setParentNode(UnionFindNode<T> parentNode) {
-        this.parentNode = parentNode;
-    }
-
-    public int getRank() {
-        return rank;
-    }
-
-    public void setRank(int rank) {
-        this.rank = rank;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
     }
 }
